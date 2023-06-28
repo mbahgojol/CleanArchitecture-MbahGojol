27c7ac34b72ab138953fcdb0789eb6287cb01104
mbahgojol <profghozimahdi@gmail.com>
2023-06-29 00:17:49 +0700

    create example request with ktor

--- core/domain/src/main/java/com/mbahgojol/domain/GetNews.kt
+++ core/domain/src/main/java/com/mbahgojol/domain/GetNews.kt
@@ -1,11 +1,11 @@
 package com.mbahgojol.domain
 
 import com.mbahgojol.common.interactor.Interactor
-import com.mbahgojol.data.news.NewsRepositoryImpl
+import com.mbahgojol.data.news.NewsRepository
 import javax.inject.Inject
 
 class GetNews @Inject constructor(
-    private val newsRepository: NewsRepositoryImpl,
+    private val newsRepository: NewsRepository,
 ) : Interactor<GetNewsParams>() {
     override suspend fun doWork(params: GetNewsParams) {
         return newsRepository.getNews()
