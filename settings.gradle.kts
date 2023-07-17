41b029ec45e9bca1f8349e7c95e99d7d42ec9b4f
mbahgojol <profghozimahdi@gmail.com>
2023-07-17 15:20:30 +0700

    create convention plugin for kotlinmultiplatform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 // https://docs.gradle.org/7.6/userguide/configuration_cache.html#config_cache:stable
 // enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
-rootProject.name = "CleanArchitectureKMM"
+rootProject.name = "CleanArchitecture-Compose-Multiplatform"
 
 include(":androidApp")
 include(":presentation")
